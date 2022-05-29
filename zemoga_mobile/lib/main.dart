f409f4921318a53270ba686d8647fdd78b5ed85b
victorgt@gmail.com <Victor Gutierrez>
2022-05-29 19:01:58 -0400

    Adding screenshots

--- zemoga_mobile/lib/main.dart
+++ zemoga_mobile/lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:zemoga_mobile/posts.dart';
-import 'dart:convert';
-import 'ApiService.dart';
-import 'model/post.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,8 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: new ThemeData(primaryColor: Color.fromRGBO(58, 66, 86, 1.0)),
-      home: PostsPage(),
+      theme: ThemeData(primaryColor: const Color.fromRGBO(58, 66, 86, 1.0)),
+      home: const PostsPage(),
     );
   }
 }
